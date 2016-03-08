6322796382beaf31ee0363c0337d9b7742410793
Chris Hintz <chrishintz42@gmail.com>
2016-03-08 10:36:01 -0800

    Add Wicked Wizard to Users Controller and set-up basic views for wizard steps

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
+  include Wicked::Wizard
+  steps :signup, :biographical, :spouse, :other
 
   def index
+  end
+
+  def show
+    render_wizard
   end
 end
