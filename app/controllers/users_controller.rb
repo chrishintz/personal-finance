1922eba8978e972eed46f6d06642d5f0c9295234
Chris Hintz <chrishintz42@gmail.com>
2016-03-10 16:42:33 -0800

    Wicked wizard step path working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,21 @@ class UsersController < ApplicationController
     @user = User.new
     render_wizard
   end
+
+  def update
+    @user = User.new(user_params)
+    session[:user_id] = @user.id
+    render_wizard @user
+    #   flash.now.alert = "Username or Email is already taken"  
+  end
+
+  private
+    def user_params
+      params.require(:user).permit( :username,
+                                    :email,
+                                    :password,
+                                    :password_confirmation,
+                                    :first_name,
+                                    :last_name)
+    end
 end
