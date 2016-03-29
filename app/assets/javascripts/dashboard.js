1a91e6dda203e1cd3a6e29afba61f52b39b84c29
Chris Hintz <chrishintz42@gmail.com>
2016-03-28 20:34:01 -0700

    Add sliding navigation to landing page

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,3 +1,10 @@
+$(document).ready(function(){
+  $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+    $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+});
+
 $(function () {
   $.get("/data", function(data){
     var assetArray       = [];
